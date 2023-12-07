3d035324d2f37c7a00902ab269ebbac4f46f27af
Daniel Jaramillo Moya <da.jaramim@gmail.com>
2023-12-07 17:30:38 -0300

    feat: random colors added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const body = document.querySelector('body')
 const colorPicker = document.querySelector('#color-picker')
 const eraseBtn = document.querySelector('#erase-btn')
 const clearBtn = document.querySelector('#clear-btn')
-colorPicker.value = '#000000'
+const randomBtn = document.querySelector('#random-btn')
+const defaultColor = '#000000'
+let isRandomMode = false;
+
+colorPicker.value = defaultColor
 let gridChildren;
 let colorSelected = colorPicker.value;
 let isMouseDown = false
+let isSquarePainted = false
 
 showGrid()
 
@@ -20,17 +25,22 @@ function eventListener() {
         isMouseDown = false;
     })
     body.addEventListener('mousemove', changeColor);
-    body.addEventListener('click', changeColor);
 
-    eraseBtn.addEventListener('click', eraseSquare)
+    body.addEventListener('mouseout', (e) => {
+        if (e.target.classList.contains('square')) {
+            isSquarePainted = false;
+        }
+    });
+    body.addEventListener('mousedown', changeColor);
 
-    clearBtn.addEventListener('click', clearSquares)
+    randomBtn.addEventListener('click', randomMode);
 
+    eraseBtn.addEventListener('click', eraseSquare);
 
+    clearBtn.addEventListener('click', clearSquares);
 }
 
 function showGrid() {
-
     for (let i = 0; i < 16; i++) {
         const row = document.createElement('div');
         for (let j = 0; j < 16; j++) {
@@ -44,26 +54,46 @@ function showGrid() {
 }
 
 function changeColor(e) {
-    if(!e.target.classList.contains('square')) return
-    if (isMouseDown || (!isMouseDown && e.type === 'click')) {
+    if (!e.target.classList.contains('square')) return
+    if (isRandomMode) {
+        colorSelected = getRandomColor()
+    }
+    if (isMouseDown && !isSquarePainted) {
         e.target.style.backgroundColor = colorSelected;
+        isSquarePainted = true
     }
 }
+
 function changeColorSelected() {
-    colorSelected = colorPicker.value
+    colorSelected = colorPicker.value;
 }
+
 function eraseSquare() {
-    colorPicker.value = '#ffffff'
-    changeColorSelected()
+    colorPicker.value = '#ffffff';
+    changeColorSelected();
 }
+
 function clearSquares() {
     gridChildren.forEach(rows => {
-        const columns = Array.from(rows.childNodes)
+        const columns = Array.from(rows.childNodes);
         columns.forEach(square => {
             if (square.classList.contains('square')) {
-                square.style.backgroundColor = '#ffffff'
+                square.style.backgroundColor = '#ffffff';
             }
         });
     });
 
+}
+function randomMode() {
+    isRandomMode = !isRandomMode;
+    if (!isRandomMode) changeColorSelected();
+}
+
+function getRandomColor() {
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let red = Math.floor(Math.random() * 256);
+
+    const randomColor = `rgb(${red}, ${green}, ${blue})`
+    return randomColor;
 }
